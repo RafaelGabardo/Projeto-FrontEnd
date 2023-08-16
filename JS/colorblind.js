41bdfa17bfc2fa44304fb0ad3647fc16980a6d3f
Rafael Gabardo <rggabardo@gmail.com>
2023-08-16 18:43:15 -0300

    Começando com as tentativas para mudar de cor e mudando o .gitignore

---
+++ JS/colorblind.js
@@ -0,0 +1,5 @@
+function noneOnClick() {
+    document.getElementsById('signin').style.backgroundcolor = 'mediumblue';
+    document.getElementsById('index').style.backgroundcolor = 'mediumblue';
+    document.getElementsById('login').style.backgroundcolor = 'mediumblue';
+}
